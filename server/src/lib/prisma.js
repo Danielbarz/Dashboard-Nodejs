89c782b8d92ef69fdfb8e2ada0e9fae440401c3d
rsotelkom-del <rsotelkom@gmail.com>
2026-01-22 21:00:20 +0700

    commit

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/client/index.js'
+import { PrismaClient } from '@prisma/client'
 
 // Ensure single PrismaClient instance across hot reloads (nodemon)
 const globalForPrisma = globalThis
